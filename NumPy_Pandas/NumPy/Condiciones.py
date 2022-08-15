69660a7b07957eead24d69fee298168b01da1267
eusmas <eus08@hotmail.com>
2022-08-15 01:15:47 -0500

    Fin NumPy

--- NumPy_Pandas/NumPy/Condiciones.py
+++ NumPy_Pandas/NumPy/Condiciones.py
@@ -1,3 +1,21 @@
 import numpy as np
 
-arr=np.arange(0,11)
+arr=np.arange(1,11)
+
+print("Arreglo  ", arr)
+
+#devuelve un array de falses y trues donde se cumpla la condicion
+indices_cond=arr>5
+
+print("Indices de condicion  ", indices_cond)
+#al poner un array de false y true dentro de otro array de valores, devuelve solo lo que esta en true
+print("Indices de condicion dentro del array ", arr[indices_cond])
+
+#De esta forma se obtiene el mismo resultado de forma mas directa
+print("Indices directos  ", arr[(arr>5)])
+
+#Se pueden poner condicionales dentro de la busqueda de indice
+print("Indices directos 2 ", arr[(arr>5) & (arr<9)])
+
+
+
---
+++ NumPy_Pandas/NumPy/Operaciones.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+arr=np.arange(1,11)
+
+print("Arreglo  ", arr)
+
+#definiendo arreglos con numpy (Ver Linea 3), se pueden aplicar operaciones directamente sobre el arreglo
+print("Arreglo  ", arr*2)
